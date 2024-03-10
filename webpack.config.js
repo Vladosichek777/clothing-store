00388776c55ec1cded35550efce9923a9a72fa10
Vladyslav Myronenko <ogurez111@gmail.com>
2024-03-10 20:31:04 +0100

    init:first commit

---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env) => {
+    return {
+        mode:env.mode ?? 'development', // if we didn't set mode - use development on default
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        output: {
+           path: path.resolve(__dirname, 'build'),
+           filename: '[name][contenthash].js',
+           assetModuleFilename: "assets/[hash][ext]",
+           clean: true // for update dist folder after changes (important)
+        },
+
+        plugins : [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src/index.html') }), //for creating html web page. we can set a lot of options in this row, or use just created page
+            new MiniCssExtractPlugin({ // for create a file style.css in build
+                filename:'[name][contenthash].css'
+            }),
+        ],
+
+        module: {
+            rules:[
+                { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }, //add typescript loader
+                {test: /\.html$/i,loader: "html-loader",}, // add loader for looking changes in html file
+                //for compiler sass in css (important)
+                 { 
+                    test: /\.(c|sa|sc)ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader, 
+                        "css-loader",
+                        { // this loader add prefix for old browsers
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions:{
+                                    plugins: [require('postcss-preset-env')]
+                                }
+                            }
+                        },
+                        "sass-loader"
+                        ],
+                 },
+                 // compile new code for old browsers(important)
+                 { 
+                    test: /\.(?:js|mjs|cjs)$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: [
+                          ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                      }
+                    }
+                  },
+                  //minimize size of images(important)
+                  { 
+                    test: /\.(gif|ico|png|jpe?g|svg)$/i,
+                    use: [
+                      {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          mozjpeg: {
+                            progressive: true,
+                          },
+                          // optipng.enabled: false will disable optipng
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                          },
+                          gifsicle: {
+                            interlaced: false,
+                          },
+                          // the webp option will enable WEBP
+                          webp: {
+                            quality: 75
+                          }
+                        }
+                      },
+                    ],
+                  },
+                  // work with fonts
+                  {
+                    test:/\.woff2?$/i,
+                    type: 'asset/resource',
+                    generator: {
+                      filename: 'assets/fonts/[name][ext]'
+                    }
+                  },
+                  //work with images
+                  {
+                    test:/\.(jpe?g|png|gif|svg)$/i,
+                    type: 'asset/resource',
+                    generator: {
+                      filename: 'assets/images/[name][ext]'
+                    }
+                  },
+                  //work with icons
+                  {
+                    test:/\.svg$/i,
+                    type: 'asset/resource',
+                    generator: {
+                      filename: 'assets/icons/[name][ext]'
+                    }
+                  }
+            ]
+        },
+        resolve : {
+            extensions: ['.ts', '.js'] // We can not write extensions at the end of import file(important)
+        },
+    }
+
+  };
